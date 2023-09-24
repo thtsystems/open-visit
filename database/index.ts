83d59043e0ff923b91e0587e429dd995f0a8aaec
Ernesto Resende <dev@ernestoresende.com>
2023-09-24 18:08:08 -0300

    feat: add base database config and schema

---
+++ database/index.ts
@@ -0,0 +1,22 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Client } from "pg";
+
+const client = (connectionString: string) => new Client({ connectionString });
+
+export const database = (connectionString: string) =>
+  drizzle(client(connectionString));
+
+export {
+  eq,
+  ne,
+  gt,
+  gte,
+  lt,
+  lte,
+  ilike,
+  and,
+  or,
+  placeholder,
+  asc,
+  desc,
+} from "drizzle-orm";
