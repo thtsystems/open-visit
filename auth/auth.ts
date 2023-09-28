c8fd2ba4756bacec58da600c9a9293bd5a51e833
Ernesto Resende <dev@ernestoresende.com>
2023-09-28 08:47:33 -0300

    feat: initialize auth service

--- auth/auth.ts
+++ auth/auth.ts
@@ -3,24 +3,40 @@ import { hono, web } from "lucia/middleware";
 import { pg } from "@lucia-auth/adapter-postgresql";
 import { Pool } from "pg";
 
+/**
+ * Generic function wrapper around Lucia's `auth` constructor
+ * The wrapper allows us to reuse the same auth configuration but
+ * pass different middlewares when necessary by building the auth
+ * client closer to business logic.
+ *
+ * @see: https://lucia-auth.com/basics/configuration/
+ */
 export function auth<T>(connectionString: string, middleware: Middleware<[T]>) {
   return lucia({
     adapter: pg(new Pool({ connectionString }), {
+      /* Maps to the name of the database tables */
       key: "user_key",
       session: "user_session",
       user: "user",
     }),
     env: "DEV",
     middleware: middleware,
+    /* Data that is returned when we query for session information.
+    The properties here need to be properly declared in `app.d.ts` */
     getUserAttributes: (userData) => {
-      return {};
+      return {
+        id: userData.id,
+        email: userData.email,
+      };
     },
     csrfProtection: false,
   });
 }
 
-// export type AuthFunction = typeof auth;
-// export type Auth = ReturnType<AuthFunction>;
+/* Export type infered from the `auth` constructor. This type
+should be passed down to `app.d.ts` declaration file in the workspace root */
+export type AuthFunction = typeof auth;
+export type Auth = ReturnType<AuthFunction>;
 export { LuciaError } from "lucia";
 
 export { hono, web };
---
+++ auth/index.ts
@@ -0,0 +1,134 @@
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { z } from "zod";
+
+import {
+  auth as authFunction,
+  hono as honoMiddleware,
+  LuciaError,
+} from "./auth.js";
+
+type Bindings = {
+  DATABASE_URL: string;
+};
+
+const signupSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+/** Build the auth client that returns the `auth` constructor from Lucia
+ * We pass the database connection string and the middleware as parameters. */
+async function buildAuthClient(env: Bindings) {
+  const connectionString = env.DATABASE_URL;
+  const auth = authFunction(connectionString, honoMiddleware());
+  return auth;
+}
+
+/** Initialize the app instance from Hono.
+ * @see: https://hono.dev/api/hono
+ */
+const app = new Hono<{ Bindings: Bindings }>();
+
+/** Setup Cross-Origin Resource Sharing (CORS) headers.
+ *
+ * @see: https://hono.dev/middleware/builtin/cors
+ *
+ * @todo: This should not be so permissive in prodution environemnts,
+ * specially if we're handling sensitive data. It will remain like this
+ * while we develop locally.
+ */
+app.use(
+  "*",
+  cors({
+    origin: "*",
+    allowMethods: ["GET", "POST", "OPTIONS"],
+    allowHeaders: ["*"],
+  })
+);
+
+/**From here on, we start the service routing logic
+ * @see: https://hono.dev/api/routing
+ */
+
+app.post("/login", async (context) => {
+  const auth = await buildAuthClient(context.env);
+  const requestBody = await context.req.json();
+  const creds = signupSchema.safeParse(requestBody);
+
+  try {
+    if (!creds.success) return context.text("BAD_PAYLOAD", 400);
+
+    if (creds.success) {
+      const { email, password } = creds.data;
+      const user = await auth.useKey("email", email.toLowerCase(), password);
+
+      const session = await auth.createSession({
+        userId: user.userId,
+        attributes: {},
+      });
+
+      return context.json(session, 200);
+    }
+  } catch (error) {
+    if (error instanceof LuciaError)
+      return context.json({ error: error.message }, 403);
+    else {
+      console.error(error);
+      return context.json({ error: "UNKNOWN_ERROR" }, 500);
+    }
+  }
+});
+
+app.post("/create", async (context) => {
+  const auth = await buildAuthClient(context.env);
+  const requestBody = await context.req.json();
+  const creds = signupSchema.safeParse(requestBody);
+
+  if (!creds.success) return context.text("BAD_PAYLOAD", 400);
+
+  if (creds.success) {
+    try {
+      const { email, password } = creds.data;
+
+      const user = await auth.createUser({
+        key: {
+          providerId: "email",
+          providerUserId: email.toLowerCase(),
+          password,
+        },
+        attributes: {
+          email,
+        },
+      });
+
+      const session = await auth.createSession({
+        userId: user.userId,
+        attributes: {},
+      });
+
+      return context.json(session, 200);
+    } catch (error) {
+      if (error instanceof LuciaError)
+        return context.json({ error: error.message }, 403);
+      else {
+        console.error(error);
+        return context.json({ error: "UNKNOWN_ERROR" }, 500);
+      }
+    }
+  }
+});
+
+export default {
+  fetch(request: Request, env: Bindings, ctx: ExecutionContext) {
+    /* Handles pre-flight request from cross-origin domains */
+    if (request.method === "OPTIONS") {
+      const response = new Response(null, {
+        status: 204,
+      });
+      return response;
+    }
+
+    return app.fetch(request, env, ctx);
+  },
+};
