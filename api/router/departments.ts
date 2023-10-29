f776bb59fd5e606d1481265800f9d375e4930847
JoaoAlem <joao.aalem@gmail.com>
2023-10-29 09:12:08 -0500

    feat(departments): add departments schema

--- api/router/departments.ts
+++ api/router/departments.ts
@@ -1,10 +1,17 @@
 import { Hono } from "hono"
 
-import { department } from "@open-visit/database/schema"
+import { department as departmentsTable } from "@open-visit/database/schema"
+import { createInsertSchema } from "drizzle-zod";
+import { z } from "zod";
 
 type Bindings = {
     DATABASE_URL: string;
 };
+
+const departmentsSchema = createInsertSchema(departmentsTable, {
+    id: z.string().uuid(),
+    companyId: z.string().uuid()
+})
 
 /* Initialize the router tree for the `/departments` route */
 export const departments = new Hono<{ Bindings: Bindings }>();
